1de0a5d539b787b37b4afecaa8cbdc6974b2bb50
Your Name <is0541hx@ed.ritsumei.ac.jp>
2019-10-21 17:26:30 +0900

    hello world

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ while True:
     if input_num == 0:
         print('プログラムを終了します')
         break
+    elif input_mum ==1:
+        print('hello world')
     # -- ここに処理を追加してみよう --
     else:
         print('該当するコマンドがありません')
